60e00efb2598b7cbd2fee1bde4baa444ca67b372
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-03-08 12:23:44 -0500

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h> //using this for the random library
+#include <time.h> //using this for the random library
+#include <unistd.h> //using this to pause the program for 5 seconds in between games/rounds
+using namespace std;
+
+
+
+void play(string userChoice, string computerChoice)
+{
+  int userPoints = 0;
+  int computerPoints = 0;
+  int tie = 0;
+  string userDecision = "yes";
+  const string choices[3] = {"Rock", "Paper", "Scissors"};
+  srand ( time(NULL) ); //initialize the random seed
+
+  while (userDecision == "yes")
+  {  
+    
+    cout << endl << "Choose one of the following: Rock (r), Paper (p), or Scissors (s) : ";
+    cin >> userChoice;  
+    string computerChoice = choices[rand() % 3]; //making a variable to store the random choice made by the computer
+
+//computer and user decision: 
+    
+    if (userChoice == "r")
+    {
+      cout << endl << "You have chosen: Rock" << endl;
+      cout << "The computer has chosen: " << computerChoice;
+    }
+    else if (userChoice == "p")
+    {
+      cout << endl << "You have chosen: Paper" << endl;
+      cout << "The computer has chosen: " << computerChoice;
+    }
+    else
+    {
+      cout << endl << "You have chosen: Scissors" << endl;
+      cout << "The computer has chosen: " << computerChoice;
+    }
+    
+    cout << endl;
+    
+    if (userChoice == "p" && computerChoice == "Paper" || userChoice == "r" && computerChoice == "Rock" || userChoice == "s" && computerChoice == "Scissors")
+    {
+      tie++;
+      cout << endl << "That's a tie!" << endl;
+    }
+      
+    else if (userChoice == "r" && computerChoice == "Scissors")
+    {
+      userPoints++;
+    }
+    else if (userChoice == "s" && computerChoice == "Paper")
+    {
+      userPoints++;
+    }
+    else if (userChoice == "p" && computerChoice == "Rock")
+    {
+      userPoints++;
+    }
+    else
+    {
+      computerPoints++;
+    }
+
+    cout << endl << "Your score: " << userPoints << endl;
+    cout << "Computer score: " << computerPoints << endl;
+    cout << "Tie: " << tie << endl;
+    
+//starting to tally the amount of points the computer and User get: 
+    
+
+//asking whether the user would like to continue:
+    
+    cout << endl << "Would you like to keep going? (yes/no) " << endl;
+    cin >> userDecision;
+    if (userDecision == "yes")
+    {
+      sleep(2); // pausing in between rounds
+      continue;
+    }
+
+//ending the game:
+    else
+    {
+      cout << endl << "Your final score: " << userPoints << endl;
+      cout << "Computer final score: " << computerPoints << endl;
+      cout << "Tie: " << tie << endl;
+      cout << endl << "!GAME OVER!" << endl;
+      break;
+    }
+  
+  }
+}
+
+
+
+void intro(string userChoice, string computerChoice)
+{
+  if (userChoice == "yes")
+  {
+    play(userChoice, computerChoice);
+  }
+  else if (userChoice == "no")
+  {
+    cout << "Too bad! You're already here!" << endl;
+    play(userChoice, computerChoice);
+  }
+}
+
+
+int main() 
+{
+  string userChoice;
+  string computerChoice;  
+  cout << "Ready to play Rock, Paper, Scissors? (yes/no): ";
+  cin >> userChoice;
+  if (userChoice == "yes" || userChoice == "no") //to make sure the user enters the right response
+  {    
+    intro(userChoice, computerChoice);
+  }
+  else
+  {    
+    cout << "You must enter a valid response" << endl;
+    main();
+  }
+  return 0; 
+}
